9d13dd74b181473b88985049c10784b6d8c47117
Amandickz <amandickz@gmail.com>
2024-12-16 18:41:26 -0300

    Envio dos arquivos alterados - 1

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,9 @@ public class Main {
                 "Iphone X,900.00,2",
                 "Samsung Galaxy 9,850.00,2"};
 
-        String path = "C:\\Users\\Amanda\\Desktop\\exe13_java\\in.txt";
+        String path = "C:\\Users\\Amanda\\Desktop\\exe13_java\\vendidos.csv";
+
+        String lineBr = null;
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
 
@@ -24,17 +26,38 @@ public class Main {
             e.printStackTrace();
         }
 
+        System.out.print("Digite o endereço do arquivo a ser lido: ");
+        scan.nextLine();
+        path = scan.nextLine();
+
         try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
-            String line = br.readLine();
+            do {
 
-            while(line != null) {
-                System.out.println(line);
-                line = br.readLine();
-            }
+                lineBr = br.readLine();
+                System.out.println(lineBr);
+
+                String[] split = lineBr.split(",");
+
+                System.out.println(split);
+
+                int cont = 0;
+                double valorTotal = 0.0;
+
+                for(String s : split) {
+                    if(cont == 1){
+                        valorTotal = Double.parseDouble(s);
+                    } else if(cont == 2){
+                        valorTotal *= valorTotal + Double.parseDouble(s);
+                    }
+                    cont++;
+                }
+            } while(lineBr != null);
 
         } catch (IOException e){
             e.printStackTrace();
+        } catch (NullPointerException e) {
+            scan.close();
         }
 
 
